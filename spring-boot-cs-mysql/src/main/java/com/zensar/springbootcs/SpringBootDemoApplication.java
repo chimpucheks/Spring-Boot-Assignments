bcac593e6a746d3b1d60785e3415f9f07ce22437
chimpucheks <vinodmuvvala772@gmail.com>
2022-06-18 11:59:36 +0530

    CouponService

---
+++ spring-boot-cs-mysql/src/main/java/com/zensar/springbootcs/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.zensar.springbootcs;
+
+
+
+public class SpringBootDemoApplication  {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Ram");
+		
+		System.out.println("Laxman");
+	}
+	
+
+}
